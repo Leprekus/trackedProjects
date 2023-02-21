5b2c3638c85dbf9d71944013342db9ac0fee38b3
leprekus <leprekus@yahoo.com>
2023-02-21 14:50:57 -0800

    styled header

--- js/medium-clone/components/Header.tsx
+++ js/medium-clone/components/Header.tsx
@@ -1,22 +1,37 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
 function Header() {
   return (
-    <header>
-        <div>
-            <Link href='/'>
-                <Image src='https://links.papareact.com/yvf' alt='logo' width={200} height={200}></Image>
-            </Link>
+    <header className='flex justify-between p-5 max-w-7xl mx-auto' >
+      <div className='flex items-center sapce-x-5'>
+        <Link href='/'>
+          <Image
+            src='https://links.papareact.com/yvf'
+            alt='logo'
+            className='w-44 object-contain cursor-pointer'
+            width={200}
+            height={200}
+          />
+        </Link>
+        <div
+          className='hidden md:inline-flex
+        items-center space-x-5'
+        >
+          <h3>About</h3>
+          <h3>Contact</h3>
+          <h3 className='text-white bg-green-600 
+          px-4 py-1 rounded-full cursor-pointer'>Follow</h3>
         </div>
-        <div>
-            <Link href='/'>
-            
-            </Link>
-        </div>
+      </div>
+
+      <div className='flex items-center space-x-5 text-green-600'>
+        <h3 className='cursor-pointer'>Sign In</h3>
+        <h3 className='cursor-pointer  border px-4 py-1 rounded-full'>Get Started</h3>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
