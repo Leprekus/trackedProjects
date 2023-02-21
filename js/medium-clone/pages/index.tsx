523e54e3fa12f15b3b5205616b4c2ffbf02edce6
leprekus <leprekus@yahoo.com>
2023-02-21 14:05:41 -0800

    create components folder and Header

--- js/medium-clone/pages/index.tsx
+++ js/medium-clone/pages/index.tsx
@@ -6,10 +6,9 @@ const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
