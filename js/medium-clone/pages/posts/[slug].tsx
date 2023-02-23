64693c5a76a4f5479a72549329500343d0952363
leprekus <leprekus@yahoo.com>
2023-02-22 17:55:19 -0800

    implement post page with isr

--- js/medium-clone/pages/posts/[slug].tsx
+++ js/medium-clone/pages/posts/[slug].tsx
@@ -1,7 +1,9 @@
 import { GetStaticPaths, GetStaticProps } from 'next'
 import React from 'react'
+import Header from '../../components/Header'
 import { sanityClient, urlFor } from '../../sanity'
 import { PostProps } from '../../typings'
+import PortableText from 'react-portable-text'
 //prebuilds routes in advance
 export const getStaticPaths:GetStaticPaths = async () => {
   const query = `*[_type == 'post'] {
@@ -59,22 +61,59 @@ export const getStaticProps:GetStaticProps = async ({ params }) => {
   return {
     props: {
       post,
-    }
+    },
+    revalidate: 60, //updates cached version after 60 seconds
   }
 }
 interface Props {
   post:PostProps
 }
-function slug({ post }:Props) {
+function Slug({ post }:Props) {
   console.log(post)
   return (
     <div>
-      <div>
-        <img src={post.mainImage} alt="" />
-      </div>
-      <h1 className='text-6xl'>{post.title}</h1>
+      <Header/>
+      <img 
+      className='w-full h-40 object-contain'
+      src={urlFor(post.mainImage)}
+      alt="banner" />
+
+      <article className='max-w-3xl mx-auto p-5'>
+        <h1 className='text-3xl mt-10 mb-3'>{post.title}</h1>
+        <h2 className='text-xl font-light text-gray-500 mb-2'>{post.description}</h2>
+        
+        <div className='flex items-center space-x-2'>
+          <img 
+          className='h-10 w-10 rounded-full'
+          src={urlFor(post.author.image)} 
+          alt="" />
+          <p className='font-extralight text-sm'>Blog post by <span className='text-green-600'>{post.author.name}</span> - Published at {new Date(post._createdAt).toLocaleString()}</p>
+        </div>
+        <div className='mt-10'>
+          <PortableText
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+            content={post.body}
+            //serializers are used to dictate how the HTML is parsed
+            serializers={{
+              h1: (props: any) => (
+                <h1 className='text-2xl font-bold my-5'{ ...props }/>
+              ),
+              h2: (props: any) => (
+                <h1 className='text-xl font-bold my-5'{ ...props }/>
+              ),
+              li: ({ children }: any) => (
+                <li className='ml-4 list-disc'>{ children }</li>
+              ),
+              ul: ({ href, children }: any) => (
+                <a href={ href } className='text-blue-500 hover:underline'>{ children }</a>
+              )
+            }}
+          />
+        </div>
+      </article>
     </div>
   )
 }
 
-export default slug
+export default Slug
