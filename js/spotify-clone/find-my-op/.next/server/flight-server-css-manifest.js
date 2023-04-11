5b990ad25bf217d2c18b0cf6807e7ea871df74ce
Leprekus <leprekus@yahoo.com>
2023-04-10 19:55:06 -0700

    delete spotify clone

---
+++ js/spotify-clone/find-my-op/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,19 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/raulrodriguez/Desktop/Fullstack/trackedProjects/js/spotify-clone/find-my-op/src/app/page.tsx": [
+      "/Users/raulrodriguez/Desktop/Fullstack/trackedProjects/js/spotify-clone/find-my-op/node_modules/next/font/google/target.css?{\"path\":\"src/app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "/Users/raulrodriguez/Desktop/Fullstack/trackedProjects/js/spotify-clone/find-my-op/src/app/layout.tsx": [
+      "/Users/raulrodriguez/Desktop/Fullstack/trackedProjects/js/spotify-clone/find-my-op/src/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/raulrodriguez/Desktop/Fullstack/trackedProjects/js/spotify-clone/find-my-op/src/app/page": [
+      "/Users/raulrodriguez/Desktop/Fullstack/trackedProjects/js/spotify-clone/find-my-op/node_modules/next/font/google/target.css?{\"path\":\"src/app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/raulrodriguez/Desktop/Fullstack/trackedProjects/js/spotify-clone/find-my-op/src/app/globals.css"
+    ],
+    "/Users/raulrodriguez/Desktop/Fullstack/trackedProjects/js/spotify-clone/find-my-op/src/app/home/page": [
+      "/Users/raulrodriguez/Desktop/Fullstack/trackedProjects/js/spotify-clone/find-my-op/src/app/globals.css"
+    ]
+  }
+}
